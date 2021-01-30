6980dc7d802635f8d34c517390fa21702cce8970
mihreteab <mihreteabdemeke@gmail.com>
2021-01-30 10:33:35 +0300

    controller practice

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  root 'static_pages#home'
+  # root 'static_pages#home'
+  root 'post#index'
   get 'static_pages/home'
   get 'static_pages/help'
   get 'static_pages/about'
   get 'static_pages/contact'
+  get 'post' => 'post#index'
+  resources :post
+  
 end
