f7859368380d2a28e4004e4db86c18b0e091ebe0
whizmihreteab <swengineer1.whizkids@gmail.com>
2021-02-06 07:36:37 +0300

    signup page partial completion

--- config/routes.rb
+++ config/routes.rb
@@ -8,10 +8,11 @@ Rails.application.routes.draw do
   # get 'static_pages/contact'
   # get 'post' => 'post#index'
   root 'static_pages#home'
+  # get 'static_pages/help'
   get '/help', to: 'static_pages#help'
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
   get '/signup', to: 'users#new'
-  # resources :post
+  resources :users
   
 end
