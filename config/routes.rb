7b3c420c379e9c6d85d21b515e8297c70f621e61
= <=>
2021-02-12 12:03:38 +0300

    Finish user eddit, update, index and destroy actions

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   # get 'static_pages/contact'
   # get 'post' => 'post#index'
   root 'static_pages#home'
-  # get 'static_pages/help'
+  # get '/users' to: 'users#index'
   get '/help', to: 'static_pages#help'
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
