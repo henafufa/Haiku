891269202edf23fbb863b4e5dd4cdfd8f587c910
= <=>
2021-02-25 15:26:20 +0300

    domain changed to heroku.com and https deleted from host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,7 +68,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'https://infinite-inlet-78397.herokuapp.com'
+  host = 'infinite-inlet-78397.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address => 'smtp.sendgrid.net',
@@ -76,7 +76,7 @@ Rails.application.configure do
     :authentication => :plain,
     :user_name => ENV['SENDGRID_USERNAME'],
     :password => ENV['SENDGRID_PASSWORD'],
-    :domain => 'https://infinite-inlet-78397.herokuapp.com',
+    :domain => 'herokuapp.com',
     :enable_starttls_auto => true
   }
 
