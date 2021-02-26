503c31b8ff7f2afe95e3c5d7eb19157ddd765054
= <=>
2021-02-26 15:52:14 +0300

    send Html confirimation email to user

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -7,7 +7,23 @@ class UserMailer < ApplicationMailer
   #
   def account_activation(user)
     @user = user
-    mail to: user.email, subject: "Account activation"
+    # mail to: user.email, subject: "Account activation"
+    api_key = ENV['MAILGUN_API_KEY']
+    mg_client = Mailgun::Client.new api_key
+    mail_from = "Micropost"
+    mail_to = user.email
+    message_params = {:from => mail_from,
+                      :to => mail_to,
+                      :subject => "Micropost User Activation",
+                      :html => " <h1>Micropost App</h1>
+                      <p>Hi <%= @user.name %>,</p>
+                        <p>
+                        Welcome to the Micropost App! Click on the link below to activate
+                        your account:
+                        </p>
+                      <%= link_to "Activate", edit_account_activation_url(@user.activation_token, email: @user.email) %>click here"}
+    domain = ENV['MAILGUN_DOMAIN']
+    mg_client.send_message domain, message_params
   end
 
   # Subject can be set in your I18n file at config/locales/en.yml
