3a83ef15669fc617c5fa664ceb7db4c093dc7d90
Daniel Zelalem <danielzelalemheru@gmail.com>
2021-03-04 16:20:43 +0300

    Private haiku posting works

--- app/controllers/haikus_controller.rb
+++ app/controllers/haikus_controller.rb
@@ -3,7 +3,15 @@ class HaikusController < ApplicationController
     before_action :correct_user, only: :destroy
 
     def create
-        @haiku = current_user.haikus.build(haiku_params)        
+        verse_1 = params[:haiku][:verse_1]
+        verse_2 = params[:haiku][:verse_2]
+        verse_3 = params[:haiku][:verse_3]
+        is_public = true
+        if params[:visibility] && params[:visibility] === 'Private'
+            is_public = false
+        end
+
+        @haiku = current_user.haikus.build(verse_1: verse_1, verse_2: verse_2, verse_3: verse_3, public: is_public)        
         if @haiku.save
             flash[:success] = "Haiku created!"
             redirect_to root_url
@@ -25,7 +33,10 @@ class HaikusController < ApplicationController
 
     private
         def haiku_params
-            params.require(:haiku).permit(:verse_1, :verse_2, :verse_3)
+            p 'parmas'
+            p '00000000000000000000000000000000000000000000000000000122'
+            
+            params.require(:haiku).permit(:verse_1, :verse_2, :verse_3, :visibility)
         end
         def correct_user
             @haiku = current_user.haikus.find_by(id: params[:id])
