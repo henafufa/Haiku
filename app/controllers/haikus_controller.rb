04de11b1fb4eec956830c0888e9170623cf1a037
= <=>
2021-03-05 09:39:26 +0300

    public to view only public and private to view only privae

--- app/controllers/haikus_controller.rb
+++ app/controllers/haikus_controller.rb
@@ -20,7 +20,8 @@ class HaikusController < ApplicationController
             @micropost = Micropost.new
 
             @feed_items = current_user.feed.paginate(:page => params[:page], :per_page => 5, :total_entries => 30)
-            @haiku_feed_items = current_user.haiku_feed.paginate(:page => params[:page], :per_page => 5, :total_entries => 30)
+            # @haiku_feed_items = current_user.haiku_feed.paginate(:page => params[:page], :per_page => 5, :total_entries => 30)
+            @haiku_feed_items = Haiku.where("user_id = ? and public = ?", current_user.id, true).paginate(:page => params[:page], :per_page => 5, :total_entries => 30)
             render 'static_pages/home'
         end
     end
