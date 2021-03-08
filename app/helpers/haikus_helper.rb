359157bee8fcc9f418f36dd36412582d2c9d2cf2
Daniel Zelalem <danielzelalemheru@gmail.com>
2021-03-08 10:09:15 +0300

    Syllables Counter added

--- app/helpers/haikus_helper.rb
+++ app/helpers/haikus_helper.rb
@@ -1,2 +1,20 @@
 module HaikusHelper
+   
+    def SyllableCount(text)
+        s = "->s{s.scan(/[aiouy]+e*|e(?!d$|ly).|[td]ed|le$/).size}"
+        f = eval s
+        f.call(text)
+    end
+
+    def verse_1_haiku?(verse)
+        SyllableCount(verse) == 5 || SyllableCount(verse) == 4 || SyllableCount(verse) == 6
+    end
+
+    def verse_2_haiku?(verse)
+        SyllableCount(verse) == 7 || SyllableCount(verse) == 8 || SyllableCount(verse) == 6
+    end
+
+    def verse_3_haiku?(verse)
+        SyllableCount(verse) == 5 || SyllableCount(verse) == 4 || SyllableCount(verse) == 6
+    end
 end
--- test/models/haiku_test.rb
+++ test/models/haiku_test.rb
@@ -6,7 +6,7 @@ class HaikuTest < ActiveSupport::TestCase
   # end
   def setup
     @user = users(:michael)
-    @haiku = @user.haikus.build(verse_1: "Lorem ipsum", verse_2: "Lorem ipsum", verse_3: "Lorem ipsum", user_id: @user.id, tag:"funny")
+    @haiku = @user.haikus.build(verse_1: "cafe patio", verse_2: "above the cacophony", verse_3: "cafe patio", user_id: @user.id, tag:"funny")
     # This code is not idiomatically correct.
     # @haiku = Haiku.new(verse_1: "Lorem ipsum", verse_2: "Lorem ipsum", verse_3: "Lorem ipsum", user_id: @user.id)
   end
@@ -68,9 +68,10 @@ class HaikuTest < ActiveSupport::TestCase
   end
   
 
-  test "associated comment should be destroyed" do
+  test "associated comment should be destroyed" do\
+    
     @haiku.save
-    @haiku.haiku_comments.create!(verse_1: "Lorem ipsum", verse_2: "Lorem ipsum", verse_3: "Lorem ipsum", user_id: users(:michael).id)
+    @haiku.haiku_comments.create!(verse_1: "cafe patio", verse_2: "above the cacophony", verse_3: "cafe patio", user_id: users(:michael).id)
     assert_difference 'HaikuComment.count', -1 do
       @haiku.destroy
     end
