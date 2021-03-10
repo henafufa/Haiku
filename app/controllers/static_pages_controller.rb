e7c96d034618fe0bfb841974a20949f4bf05908a
Hena f <honeyfuf12@gmail.com>
2021-03-09 23:44:41 -0800

    condition added to system remainder

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,12 @@
 class StaticPagesController < ApplicationController
   def home
-    ActionCable.server.broadcast('remainder_channel','Hi mira, dont forgot to post your haiku')
+   
     if logged_in?
+      @postStatus = DailyChallenge.where("user_id = ? and thirtyDates LIKE ? ", current_user.id, "%#{Time.zone.now.to_date}%")
+      p "postStatus#{@postStatus.first.postStatus}"
+      if current_user.challenge_mode && !@postStatus.first.postStatus
+       ActionCable.server.broadcast('remainder_channel',"Hi #{@current_user.name}, You didin't post today, dont forgot to post your haiku!")
+      end
       @reaction = Reaction.new
       @comment = Comment.new
       @haiku_comment = HaikuComment.new
