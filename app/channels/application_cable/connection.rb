162e240d2b09ada25b6b8b8171a5aa86a9fc3ed7
Hena f <honeyfuf12@gmail.com>
2021-03-18 07:26:47 -0700

    Disabling ActionCable for Unauthenticated Users

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -3,7 +3,7 @@ module ApplicationCable
     identified_by :current_user
 
     def connect
-      self.current_user = current_user
+      self.current_user = find_verified_user
       logger.add_tags 'ActionCable', current_user.name
     end
 
