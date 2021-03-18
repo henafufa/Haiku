ac3fd5cdde89061ecdd7ec84982c9b97352658a7
Hena f <honeyfuf12@gmail.com>
2021-03-18 06:08:12 -0700

    Disabling ActionCable for Unauthenticated Users

--- app/javascript/channels/remainder_channel.js
+++ app/javascript/channels/remainder_channel.js
@@ -1,26 +1,29 @@
 import consumer from "./consumer"
 
-// if ($("meta[name= 'current-user']").length > 0 ){
-  consumer.subscriptions.create("RemainderChannel", {
-    connected() {
-      console.log("connected");
-      // Called when the subscription is ready for use on the server
-    },
+document.addEventListener('turboLinks:load',()=>{
 
-    disconnected() {
-      console.log("disconnected");
-      // Called when the subscription has been terminated by the server
-    },
-
-    received(data) {
-      // Called when there's incoming data on the websocket for this channel
-      console.log("recieved");
-      if (Notification.permission === 'granted'){
-        var title= 'Haiku Remainder'
-        var body= data
-        var options= {body: body}
-        new Notification(title, options)
+  // if ($("meta[name= 'current-user']").length > 0 ){
+    consumer.subscriptions.create({channel: "RemainderChannel",user_id: 79}, {
+      connected() {
+        console.log("connected to channel");
+        // Called when the subscription is ready for use on the server
+      },
+  
+      disconnected() {
+        console.log("disconnected");
+        // Called when the subscription has been terminated by the server
+      },
+  
+      received(data) {
+        // Called when there's incoming data on the websocket for this channel
+        console.log(data);
+        if (Notification.permission === 'granted'){
+          var title= 'Haiku Remainder'
+          var body= data
+          var options= {body: body}
+          new Notification(title, options)
+        }
       }
-    }
-  });
-// }
+    });
+  // }
+})
