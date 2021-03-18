073d7bf62af14c428f41c3f8364128ea89e624dd
Hena f <honeyfuf12@gmail.com>
2021-03-18 04:07:58 -0700

    authencticate user for notification

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -3,19 +3,19 @@ module ApplicationCable
     identified_by :current_user
 
     def connect
-      self.current_user = find_verified_user
+      self.current_user = current_user
       logger.add_tags 'ActionCable', current_user.name
     end
 
-    protected
+    # protected
 
-    def find_verified_user
-      verified_user = User.find_by(id: cookies.signed['user.id'])
-      if verified_user && cookies.signed['user.expires_at'] > Time.now
-        verified_user
-      else
-        reject_unauthorized_connection
-      end
-    end
+    # def find_verified_user
+    #   verified_user = User.find_by(id: cookies.signed['user_id'])
+    #   if verified_user && cookies.signed['user.expires_at'] > Time.now
+    #     verified_user
+    #   else
+    #     reject_unauthorized_connection
+    #   end
+    # end
   end
 end
