76c23fdbbc7785c54a32c0c050add84aadc8eefa
hena <honeyfu12@gmail.com>
2021-03-19 20:49:36 +0300

    update on authenticating user for notification

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -3,15 +3,15 @@ module ApplicationCable
     identified_by :current_user
 
     def connect
-      self.current_user = find_verified_user
+      self.current_user = find_notified_user
       logger.add_tags 'ActionCable', current_user.name
     end
 
     protected
 
-    def find_verified_user
-      verified_user = User.find_by(id: cookies.encrypted[:user_id])
-      p "$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$verified_user----------------------#{verified_user}"
+    def find_notified_user
+      notified_user = User.find_by(id: cookies.encrypted[:user_id])
+      p "$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$verified_user----------------------#{notified_user}"
       if current_user = User.find_by(id: cookies.encrypted[:user_id])
         current_user 
     else
