9e111ef55729b3c512acbc238c112a96b4f044f0
hena <honeyfu12@gmail.com>
2021-03-19 13:10:24 +0300

    authenticating actionCable with correct user

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -10,9 +10,9 @@ module ApplicationCable
     protected
 
     def find_verified_user
-      verified_user = User.find_by(id: cookies.signed['user.id'])
-      if verified_user && cookies.signed['user.expires_at'] > Time.now
-        verified_user
+      # verified_user = User.find_by(id: cookies.signed['user.id'])
+      if current_user
+        current_user
       else
         reject_unauthorized_connection
       end
