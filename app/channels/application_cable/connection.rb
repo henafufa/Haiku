f61ea568c73661b21d8f5a1e771662becdc5025a
Hena f <honeyfuf12@gmail.com>
2021-03-20 00:12:29 -0700

    add other way to authenticate correct user for notification

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,22 +1,22 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
-    identified_by :current_user
+    # identified_by :current_user
 
-    def connect
-      self.current_user = find_notified_user
-      logger.add_tags 'ActionCable', current_user.name
-    end
+    # def connect
+    #   self.current_user = find_notified_user
+    #   logger.add_tags 'ActionCable', current_user.name
+    # end
 
-    protected
+    # protected
 
-    def find_notified_user
-      notified_user = User.find_by(id: cookies.encrypted[:user_id])
-      p "$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$verified_user----------------------#{notified_user}"
-      if current_user = User.find_by(id: cookies.encrypted[:user_id])
-        current_user 
-    else
-        reject_unauthorized_connection 
-    end 
-    end
+    # def find_notified_user
+    #   notified_user = User.find_by(id: cookies.encrypted[:user_id])
+    #   p "$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$verified_user----------------------#{notified_user}"
+    #   if current_user = User.find_by(id: cookies.encrypted[:user_id])
+    #     current_user 
+    # else
+    #     reject_unauthorized_connection 
+    # end 
+    # end
   end
 end
