f5837a8326880e783d1f6244025d5125d47307ff
Hena f <honeyfuf12@gmail.com>
2021-03-20 00:49:32 -0700

    authenticaticating user for notification

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,22 +1,27 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
-    # identified_by :current_user
+    identified_by :current_user
 
-    # def connect
-    #   self.current_user = find_notified_user
-    #   logger.add_tags 'ActionCable', current_user.name
-    # end
+    def connect
+      self.current_user = find_notified_user
+      logger.add_tags 'ActionCable', current_user.name
+    end
 
-    # protected
+    protected
 
-    # def find_notified_user
-    #   notified_user = User.find_by(id: cookies.encrypted[:user_id])
-    #   p "$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$verified_user----------------------#{notified_user}"
-    #   if current_user = User.find_by(id: cookies.encrypted[:user_id])
-    #     current_user 
-    # else
-    #     reject_unauthorized_connection 
-    # end 
-    # end
+    def find_notified_user
+      # notified_user = User.find_by(id: cookies.encrypted[:user_id])
+      notified_user = User.find_by(id: session[:user_id])
+      p "$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$verified_user----------------------#{notified_user}"
+      if (user_id = session[:user_id])
+        current_user = User.find_by(id: session[:user_id])
+        current_user
+      elsif (user_id = cookies.encrypted[:user_id])
+        current_user = User.find_by(id: cookies.encrypted[:user_id])
+        current_user
+      else
+        reject_unauthorized_connection 
+      end
+    end
   end
 end
