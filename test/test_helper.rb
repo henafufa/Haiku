25b01c234987cbc906ca2ff9c74777069fc9db2a
Daniel Zelalem <danielzelalemheru@gmail.com>
2021-03-22 17:18:43 +0300

    profile-bug-fix

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -25,4 +25,8 @@ class ActionDispatch::IntegrationTest
   def log_in_as(user, password: 'password', remember_me: '1')
     post login_path, params: { session: { email: user.email, password: password, remember_me: remember_me } }
   end
+
+  def get_activities(user) 
+    return PublicActivity::Activity.order('created_at desc').where(owner: user)
+  end
 end
